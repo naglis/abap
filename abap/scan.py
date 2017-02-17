f0b9782b46f1f12db40e9592d32a548713d8d3bb
Naglis Jonaitis <naglis@mailbox.org>
2017-02-17 14:56:34 +0200

    abap: revork scanner to operate on pathlib.Paths

--- abap/scan.py
+++ abap/scan.py
@@ -1,15 +1,26 @@
-import os
-import collections
+import itertools
+import pathlib
+
+from abap import utils
 
 
-def labeled_scan(path: str, label_funcs, path_join=os.path.join):
-    results = collections.defaultdict(list)
-    for subdir, dirs, files in os.walk(path):
-        rel_dir = os.path.relpath(subdir, path)
-        rel_dir = '' if rel_dir == '.' else rel_dir
-        for fn in files:
+def labeled_scan(path: pathlib.Path, label_funcs):
+    return {
+        k: list(map(utils.second_of, g))
+        for k, g in itertools.groupby(
+            sorted(labeled_scan_iter(path, label_funcs), key=utils.first_of),
+            key=utils.first_of,
+        )
+    }
+
+
+def labeled_scan_iter(path: pathlib.Path, label_funcs):
+    for child in path.iterdir():
+        if child.is_dir():
+            yield from labeled_scan_iter(child, label_funcs)
+        elif child.is_file():
             for label, func in label_funcs.items():
-                if func(fn):
-                    results[label].append(path_join(rel_dir, fn))
-
-    return results
+                if func(str(child)):
+                    yield label, child
+        else:
+            pass
