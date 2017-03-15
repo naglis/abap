5f77fd8de88fe89eeac7f725e3d0aa0c6d990315
Naglis Jonaitis <naglis@mailbox.org>
2017-03-16 00:49:01 +0200

    tests, tox.ini: rewrite tests using py.test

--- tests/test_abook.py
+++ tests/test_abook.py
@@ -1,26 +1,21 @@
-import unittest
+import pytest
 
 from abap.abook import Duration
 
 
-class TestDuration(unittest.TestCase):
+@pytest.mark.parametrize('input, expected', [
+    ('00:00:01', 1_000),
+    ('00:01:01', 61_000),
+    ('00:01:01.001', 61_001),
+])
+def test_duration_from_string(input, expected):
+    assert Duration.from_string(input).duration == expected
 
-    def test_from_string(self):
-        test_cases = [
-            ('00:00:01', 1_000),
-            ('00:01:01', 61_000),
-            ('00:01:01.001', 61_001),
-        ]
-        for s, expected in test_cases:
-            with self.subTest(s=s, expected=expected):
-                self.assertEqual(Duration.from_string(s).duration, expected)
 
-    def test_format(self):
-        test_cases = [
-            (1, '{d:h:m:s}', '00:00:01'),
-            (61, '{d:h:m:s}', '00:01:01'),
-            (61, '{d:h:m:s.ms}', '00:01:01.000'),
-        ]
-        for duration, fmt, expected in test_cases:
-            with self.subTest(duration=duration, fmt=fmt, expected=expected):
-                self.assertEqual(fmt.format(d=Duration(duration)), expected)
+@pytest.mark.parametrize('input, fmt, expected', [
+    (1, '{d:h:m:s}', '00:00:01'),
+    (61, '{d:h:m:s}', '00:01:01'),
+    (61, '{d:h:m:s.ms}', '00:01:01.000'),
+])
+def test_duration_format(input, fmt, expected):
+    assert fmt.format(d=Duration(input)) == expected
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,55 +1,46 @@
-import unittest
+import pytest
 
 from abap import utils
 
 
-class TestUtils(unittest.TestCase):
+@pytest.mark.parametrize('input, expected', [
+    ('Labas', 'labas'),
+    ('labasą', 'labas'),
+    ('ląbas', 'l_bas'),
+])
+def test_slugify(input, expected):
+    assert utils.slugify(input) == expected
 
-    def test_slugify(self):
-        test_cases = [
-            ('Labas', 'labas'),
-            ('labasą', 'labas'),
-            ('ląbas', 'l_bas'),
-        ]
-        for s, expected in test_cases:
-            with self.subTest(s=s, expected=expected):
-                actual = utils.slugify(s)
-                self.assertEqual(actual, expected)
 
-    def test_parse_duration(self):
-        test_cases = [
-            ('1:12', None, 72000),
-            ('12', None, 12000),
-            ('1:12:13', None, 4333000),
-            ('1:12:13.123', None, 4333123),
-            ('1:12:13,123', ValueError, None),
-            ('1:1:12:13', ValueError, None),
-            ('1:a:13', ValueError, None),
-            ('a', ValueError, None),
-            ('', None, 0),
-            (None, None, 0),
-            ('12.345', None, 12345),
-            ('12.34567', None, 12346),
-        ]
-        for s, exc, expected in test_cases:
-            with self.subTest(s=s, exc=exc, expected=expected):
-                if exc:
-                    with self.assertRaises(exc):
-                        actual = utils.parse_duration(s)
-                else:
-                    actual = utils.parse_duration(s)
-                    self.assertEqual(actual, expected)
+@pytest.mark.parametrize('input, error_cls, expected', [
+    ('1:12', None, 72000),
+    ('12', None, 12000),
+    ('1:12:13', None, 4333000),
+    ('1:12:13.123', None, 4333123),
+    ('1:12:13,123', ValueError, None),
+    ('1:1:12:13', ValueError, None),
+    ('1:a:13', ValueError, None),
+    ('a', ValueError, None),
+    ('', None, 0),
+    (None, None, 0),
+    ('12.345', None, 12345),
+    ('12.34567', None, 12346),
+])
+def test_parse_duration(input, error_cls, expected):
+    if error_cls:
+        with pytest.raises(error_cls):
+            utils.parse_duration(input)
+    else:
+        assert utils.parse_duration(input) == expected
 
-    def test_validate_lang_code(self):
-        test_cases = [
-            ('en', True),
-            ('en-US', True),
-            ('en-cockney', True),
-            ('i-navajo', True),
-            ('x-klingon', True),
-            ('Foo,bar', False),
-        ]
-        for lang_code, expected in test_cases:
-            with self.subTest(lang_code=lang_code, expected=expected):
-                actual = utils.validate_lang_code(lang_code)
-                self.assertEqual(actual, expected)
+
+@pytest.mark.parametrize('input, expected', [
+    ('en', True),
+    ('en-US', True),
+    ('en-cockney', True),
+    ('i-navajo', True),
+    ('x-klingon', True),
+    ('Foo,bar', False),
+])
+def test_validate_lang_code(input, expected):
+    assert utils.validate_lang_code(input) == expected
